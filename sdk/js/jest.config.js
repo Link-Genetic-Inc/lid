a8a71a990941c38f28f618724b78e57116ebb368
Link Genetic <c.n@linkgenetic.com>
2026-03-01 08:34:06 +0100

    Create jest.config.js

---
+++ sdk/js/jest.config.js
@@ -0,0 +1,18 @@
+/** @type {import('jest').Config} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ['<rootDir>/src'],
+    testMatch: ['**/__tests__/**/*.ts', '**/*.test.ts', '**/*.spec.ts'],
+    transform: {
+          '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.json' }],
+    },
+    collectCoverageFrom: [
+          'src/**/*.ts',
+          '!src/**/*.d.ts',
+          '!src/**/__tests__/**',
+        ],
+    coverageDirectory: 'coverage',
+    coverageReporters: ['text', 'lcov', 'html'],
+    moduleFileExtensions: ['ts', 'js', 'json'],
+};
